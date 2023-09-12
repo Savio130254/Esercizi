5164e11f7e4808e895521b615cb174f7d21a37e4
savio <s.matera16@studenti.uniba.it>
2023-09-12 15:13:19 +0200

    aggiunto s.o.p

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input =new Scanner(System.in);
-        /*String s= input.nextLine();
-        System.out.println("La stringa ha lunghezza dispari?" + Esercizio1.esercizio1(s));
+        Scanner input = new Scanner(System.in);
+        /*String s = input.nextLine();
+        System.out.println("La stringa ha lunghezza dispari? " + Esercizio1.esercizio1(s));*/
 
         System.out.print("inserisci una stringa da invertire\n");
         String parola=input.nextLine();
@@ -22,11 +22,13 @@ public class Main {
         System.out.println("il"+ Esercizio3.fattoriale_iterativo(number));
         System.out.println("inserisci");
         number = input.nextInt();
-        System.out.println("il"+ Esercizio3.fattoriale_ricorsivo(number));*/
+        System.out.println("il"+ Esercizio3.fattoriale_ricorsivo(number));
 
         System.out.println("inserisci i secondi");
-        int number = input.nextInt();
-        System.out.println(Esercizio4.esercizio4(number));
+        int n = input.nextInt();
+        System.out.println(Esercizio4.esercizio4(n));
+        System.out.println("ciaoooo");
+
 
     }
 }
